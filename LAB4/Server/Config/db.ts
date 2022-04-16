233a9a10a34fb03a5359eb757d263b3266d5eda0
David N <dtndccp2020@outlook.com>
2022-04-16 18:11:10 -0400

    fix background image error

--- LAB4/Server/Config/db.ts
+++ LAB4/Server/Config/db.ts
@@ -1,5 +1,6 @@
 export const LocalURI="mongodb://127.0.0.1:27017/contacts";
-export const RemoteURI="mongodb+srv://thomas:a4PNmRUL0OuYytqo@cluster0.uflzp.mongodb.net/contacts?retryWrites=true&w=majority"
+//export const RemoteURI="mongodb+srv://thomas:a4PNmRUL0OuYytqo@cluster0.uflzp.mongodb.net/contacts?retryWrites=true&w=majority"
+export const RemoteURI= "mongodb+srv://dtnat842:SELkUTbhYAYLbNK5@cluster0.efz8k.mongodb.net/contacts?retryWrites=true&w=majority"
 export const SessionSecret = "WEBD6201SessionSecret";
 //export const Host = "localhost";
 export const Host = "MongoDB Atlas";
