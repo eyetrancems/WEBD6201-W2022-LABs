e8434665ad2a31751c8065a186d71a6ff1b4e65a
David N <dtndccp2020@outlook.com>
2022-04-16 17:04:59 -0400

    LAB4new

---
+++ LAB4/Server/Routes/contact-list.ts
@@ -0,0 +1,32 @@
+import express from 'express';
+const router = express.Router();
+
+import { AuthGuard } from '../Util/index';
+
+// import controller instance
+import {DisplayAddPage, DisplayContactListPage, DisplayEditPage, ProcessAddPage, ProcessDeletePage, ProcessEditPage } from '../Controllers/contact-list';
+
+/*********************************** CONTACT-LIST ROUTES ***************************/
+
+/* GET contact-list page. */
+router.get('/contact-list', AuthGuard, DisplayContactListPage);
+
+/* Display the Add Page */
+router.get('/add', AuthGuard, DisplayAddPage);
+
+/* Process the Add Request */
+router.post('/add', AuthGuard, ProcessAddPage);
+
+/* Display the Edit Page with Data injected from the db */
+router.get('/edit/:id', AuthGuard, DisplayEditPage);
+
+/* Process the Edit request */
+router.post('/edit/:id', AuthGuard, ProcessEditPage);
+
+/* Process the Delete request */
+router.get('/delete/:id', AuthGuard, ProcessDeletePage);
+
+
+
+/*************************************************************************/
+export default router;
---
+++ LAB4/Server/Util/index.ts
@@ -0,0 +1,43 @@
+import express, { Request, Response, NextFunction } from 'express';
+
+// enable jwt
+import jwt from 'jsonwebtoken';
+import * as DBConfig from '../Config/db';
+
+// convenience function to return the DisplayName of the user
+export function UserDisplayName(req: Request): string
+{
+    if(req.user)
+    {
+        let user = req.user as UserDocument;
+        return user.DisplayName.toString();
+    }
+    return '';
+}
+
+// custom authentication guard middleware
+export function AuthGuard(req: Request, res: Response, next: NextFunction): void
+{
+    if(!req.isAuthenticated())
+    {
+        return res.redirect('/login');
+    }
+    next();
+}
+
+export function GenerateToken(user: UserDocument): string
+{
+    const payload =
+    {
+        id: user._id,
+        DisplayName: user.DisplayName,
+        EmailAddress: user.EmailAddress,
+        username: user.username
+    }
+
+    const jwtOptions = 
+    {
+        expiresIn: 604800 // Time in ms = 1 week
+    }
+    return jwt.sign(payload, DBConfig.SessionSecret, jwtOptions);
+}
